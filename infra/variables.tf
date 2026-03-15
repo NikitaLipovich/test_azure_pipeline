8785719816cbe13678bd68275bc4a5b632e8a628
Nikita Lipovich <nikita1lipovich@gmail.com>
2026-03-14 23:53:20 -0700

    fix: pass ARM_SUBSCRIPTION_ID env var for azurerm v4 provider

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,3 @@
-variable "subscription_id" {
-  type        = string
-  description = "Azure Subscription ID."
-  sensitive   = true
-}
-
 variable "location" {
   type        = string
   description = "Azure region where resources will be deployed."
