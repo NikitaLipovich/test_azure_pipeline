69bf58bf4ef5f68964d68716a87f2cb20da1a404
Nikita Lipovich <nikita1lipovich@gmail.com>
2026-03-14 23:50:49 -0700

    fix: upgrade azurerm provider to v4, add subscription_id variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,9 @@
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID."
+  sensitive   = true
+}
+
 variable "location" {
   type        = string
   description = "Azure region where resources will be deployed."
